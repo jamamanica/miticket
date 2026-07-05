10f3beba0b405424109dd27eceddea9d0827d9e0
jamamanica <jamamanica@unjbg.edu.pe>
2026-07-05 11:07:47 -0500

    Cambio para que index.php reciba todas las peticiones y decida que mostrar

--- sistema_web/index.php
+++ sistema_web/index.php
@@ -1,41 +1,13 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
+session_start();
+require_once 'config.php';
+require_once 'modelo/Conexion.php';
 
-<head>
-    <meta charset="UTF-8">
-    <title>Registro de Usuario</title>
-</head>
-
-<body>
-
-<h2>Registrar Usuario</h2>
-
-<form action="guardar_usuario.php" method="POST">
-
-    DNI<br>
-    <input type="text" name="dni" maxlength="8" required><br><br>
-
-    Nombre<br>
-    <input type="text" name="nombre" required><br><br>
-
-    Apellido<br>
-    <input type="text" name="apellido" required><br><br>
-
-    Correo<br>
-    <input type="email" name="correo" required><br><br>
-
-    Contraseña<br>
-    <input type="password" name="contrasena" required><br><br>
-
-    Teléfono<br>
-    <input type="text" name="telefono"><br><br>
-
-    Fecha Registro<br>
-    <input type="date" name="fecha_registro" required><br><br>
-
-    <input type="submit" value="Guardar">
-
-</form>
-
-</body>
-</html>
+if (!isset($_SESSION['usuario_dni'])) {
+    header("Location: vista/usuarios/auth.php");
+    exit();
+} else {
+    header("Location: vista/home/index.php");
+    exit();
+}
+?>
