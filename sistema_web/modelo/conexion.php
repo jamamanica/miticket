a42ca244176eb0fb35b2ddc585ffa5e0cf411c42
jamamanica <jamamanica@unjbg.edu.pe>
2026-07-05 10:23:29 -0500

    Movio archivo conexion.php al directorio modelo

---
+++ sistema_web/modelo/conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$usuario = "root";
+$password = "";
+$bd = "DBMITICKET";
+
+$conn = new mysqli($host, $usuario, $password, $bd);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
+
+?>
