55774435e0621ab64f900d29bf4ae1c9e6383db2
jamamanica <jamamanica@unjbg.edu.pe>
2026-07-05 12:00:41 -0500

    Solucion de error por declaracion de variable

--- sistema_web/controlador/UsuarioController.php
+++ sistema_web/controlador/UsuarioController.php
@@ -39,7 +39,7 @@ if ($action === 'login') {
     $correo = $_POST['correo'];
     $contrasena = $_POST['contrasena'];
 
-    $usuario = Usuario::initSesion($correo, $contrasena);
+    $usuario = Usuario::iniciarSesion($correo, $contrasena);
 
     if ($usuario) {
         $_SESSION['usuario_dni'] = $usuario['dni'];
